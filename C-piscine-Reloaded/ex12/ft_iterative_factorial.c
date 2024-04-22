3f173ef0306c7a21cf443ab7fa08b0fc3811dec7
cemreozen <152500554+cemreozen@users.noreply.github.com>
2024-04-22 18:06:53 +0200

    Update ft_iterative_factorial.c

--- C-piscine-Reloaded/ex12/ft_iterative_factorial.c
+++ C-piscine-Reloaded/ex12/ft_iterative_factorial.c
@@ -17,20 +17,17 @@ int	ft_iterative_factorial(int nb)
 {
 	int	result;
 
-	result = nb;
+	result = 1;
+	if (nb < 0)
+		return (0);
 	if (nb == 1 || nb == 0)
 		return (1);
-	if (nb > 0)
+	while (nb > 1)
 	{
-		while (nb > 1)
-		{
-			result *= (nb - 1);
-			nb--;
-		}
-		return (result);
+		result *= nb;
+		nb--;
 	}
-	else
-		return (0);
+	return (result);
 }
 
 /*
